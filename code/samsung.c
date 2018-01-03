2e077af06275fa29fbbd6ba8466df4e69aa3928a
ErickCampos <erick.c.modesto@gmail.com>
2018-01-03 10:51:40 -0300

    Adicionado as funções de comando IR da samsung. Ainda não testei porque não tenho a tv da samsung aqui no lab

--- code/samsung.c
+++ code/samsung.c
@@ -6,7 +6,7 @@
 void mark(int us);
 void space(int us);
 
-int main() 
+void turn_On_Off() 
 {
 	int i,j;
 	char *stream = "1111000001110000001000000101111110";
@@ -36,9 +36,143 @@ int main()
 	sleep(0.5);
 	pwm_duty(0);
 	pwm_stop();
-
-	return 0;
 }
+
+
+void increase_volume(){
+
+	int i,j;
+	char *stream = ""1111000001110000011100000000111110";
+
+	pwm_export();
+	pwm_polarity();
+	pwm_stop(); //force PWM to be turned off before start
+
+	pwm_period(PERIOD);
+
+	pwm_start(); //Turn PWM on
+
+	for(j=0; j<6; j++) {
+		mark(4500);
+		space(4500);
+		for(i=1; i<34; i++) {
+			mark(560);
+			if(stream[i]=='1') {
+				space(1690);
+			} else {
+				space(560);
+			}
+		}
+		usleep(200000); //200ms, since signal lasts 108ms
+	}
+
+	sleep(0.5);
+	pwm_duty(0);
+	pwm_stop();
+
+}
+
+void decrease_volume(){
+
+	int i,j;
+	char *stream = "1111000001110000011010000001011110";
+
+	pwm_export();
+	pwm_polarity();
+	pwm_stop(); //force PWM to be turned off before start
+
+	pwm_period(PERIOD);
+
+	pwm_start(); //Turn PWM on
+
+	for(j=0; j<6; j++) {
+		mark(4500);
+		space(4500);
+		for(i=1; i<34; i++) {
+			mark(560);
+			if(stream[i]=='1') {
+				space(1690);
+			} else {
+				space(560);
+			}
+		}
+		usleep(200000); //200ms, since signal lasts 108ms
+	}
+
+	sleep(0.5);
+	pwm_duty(0);
+	pwm_stop();
+
+}
+
+void next_channel(){
+
+	int i,j;
+	char *stream = "1111000001110000001001000101101110";
+
+	pwm_export();
+	pwm_polarity();
+	pwm_stop(); //force PWM to be turned off before start
+
+	pwm_period(PERIOD);
+
+	pwm_start(); //Turn PWM on
+
+	for(j=0; j<6; j++) {
+		mark(4500);
+		space(4500);
+		for(i=1; i<34; i++) {
+			mark(560);
+			if(stream[i]=='1') {
+				space(1690);
+			} else {
+				space(560);
+			}
+		}
+		usleep(200000); //200ms, since signal lasts 108ms
+	}
+
+	sleep(0.5);
+	pwm_duty(0);
+	pwm_stop();
+
+}
+
+void previous_channel(){
+
+	int i,j;
+	char *stream = "1111000001110000000001000111101110";
+
+	pwm_export();
+	pwm_polarity();
+	pwm_stop(); //force PWM to be turned off before start
+
+	pwm_period(PERIOD);
+
+	pwm_start(); //Turn PWM on
+
+	for(j=0; j<6; j++) {
+		mark(4500);
+		space(4500);
+		for(i=1; i<34; i++) {
+			mark(560);
+			if(stream[i]=='1') {
+				space(1690);
+			} else {
+				space(560);
+			}
+		}
+		usleep(200000); //200ms, since signal lasts 108ms
+	}
+
+	sleep(0.5);
+	pwm_duty(0);
+	pwm_stop();
+}
+
+
+
+
 
 void mark(int us) {
 	pwm_duty(PERIOD/3); //set duty period to 1/3 of the PWM period <-> 33%
---
+++ code/samsung.h
@@ -0,0 +1,10 @@
+#ifndef SAMSUNG_H
+#define SAMSUMG_H
+
+void turn_on_off(void);
+void increase_volume(void);
+void decrease_volume(void);
+void next_channel(void);
+void previous_channel(void);
+
+#endif /* SAMSUMG_H */
